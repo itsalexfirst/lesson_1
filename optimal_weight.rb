f4f223d9c27ee2c91311834ac15ef39411ebff74
itsalexfirst <itsalex@yandex.ru>
2020-01-21 22:30:10 +0300

    Lesson_1

---
+++ optimal_weight.rb
@@ -0,0 +1,13 @@
+print "Введите Ваше имя: "
+pers_name = gets.chomp.capitalize
+
+print "Введите Ваш рост: "
+pers_height = gets.to_i
+
+optimal_weight = (pers_height - 110) * 1.15
+
+if optimal_weight >= 0
+  puts "#{pers_name}, Ваш идеальный вес #{optimal_weight}"
+else
+  puts "Ваш вес уже оптимальный"
+end
---
+++ quadratic_equation.rb
@@ -0,0 +1,21 @@
+puts "Введите коэффициенты a, b и c квадратного уравнения a*x^2+b*x+c=0"
+
+print "a="
+a = gets.to_f
+print "b="
+b = gets.to_f
+print "c="
+c = gets.to_f
+
+d = b**2 - 4 * a * c
+
+if d > 0
+  x1 = (-b + Math.sqrt(d))/(2*a)
+  x2 = (-b - Math.sqrt(d))/(2*a)
+  puts "D=#{d} x1=#{x1} x2=#{x2}"
+elsif d == 0
+  x = -b/(2*a)
+  puts "D=#{d} x=#{x}"
+else
+  puts "D=#{d} Корней нет"
+end
